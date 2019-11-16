0f3051f75eca3e6f619a6aac4af181877910a6ea
aybukeb <48856571+aybukeb@users.noreply.github.com>
2019-11-16 21:30:50 +0300

    second commit

--- app/src/main/java/com/aybukebayramic/parafy/FeedActivity.java
+++ app/src/main/java/com/aybukebayramic/parafy/FeedActivity.java
@@ -1,14 +1,62 @@
 package com.aybukebayramic.parafy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.SupportMenuInflater;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.add_expense) {
+            Intent intentToAdd=new Intent(FeedActivity.this,AddActivity.class);
+            startActivity(intentToAdd);
+
+        }
+        if(item.getItemId()==R.id.my_expense) {
+            Intent intentToExpense=new Intent(FeedActivity.this,ExpenseActivity.class);
+            startActivity(intentToExpense);
+
+
+        }
+        if(item.getItemId()==R.id.my_profil) {
+            Intent intentToProfil=new Intent(FeedActivity.this,ProfilActivity.class);
+            startActivity(intentToProfil);
+
+        }
+        if(item.getItemId()==R.id.sign_out) {
+            firebaseAuth.signOut();
+            Intent intentToSign=new Intent(FeedActivity.this,SignActivity.class);
+            startActivity(intentToSign);
+            finish();
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        firebaseAuth=FirebaseAuth.getInstance();
     }
 }
